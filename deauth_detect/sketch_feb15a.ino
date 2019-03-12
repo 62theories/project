63fb28a77586b03a613c769ca77175f4567af894
62theories <33127512+62theories@users.noreply.github.com>
2019-03-13 04:47:38 +0700

    added training version 1

--- deauth_detect/sketch_feb15a.ino
+++ deauth_detect/sketch_feb15a.ino
@@ -26,6 +26,14 @@ unsigned long ch_time { 0 };      // Last channel hop time
 int period = 0;
 bool found = false;
 
+// variables added for training experiment
+bool trainingend = false;
+int count = 0;
+int deauthCountMax = 0;
+int probeCountMax = 0;
+int beaconCountMax = 0;
+
+
 std::map<byte*,int> mem;
 std::map<byte*,int> newdata;
 std::map<byte*,int> memforprobe;
@@ -44,7 +52,7 @@ void sniffer(uint8_t *buf, uint16_t len) {
 
   // If captured packet is a deauthentication or dissassociaten frame
   if (pkt_type == 0xA0 || pkt_type == 0xC0) {
-  
+
   itr = newdata.find(addr_a); 
   if(itr == newdata.end())
   {
@@ -52,13 +60,32 @@ void sniffer(uint8_t *buf, uint16_t len) {
   }
   else
   {
-    if(itr->second+1 >= 50)
-    {
-      //Serial.print(itr->second+1);
-      //Serial.print(" ");
-      found = true;
-    }
     newdata[addr_a] = itr->second+1;
+    if(trainingend == false)
+    {
+//      Serial.println("---------------");
+//      Serial.print("traing count =");
+//      Serial.println(count);
+//      Serial.print("deauth count =");
+//      Serial.println(itr->second);
+      if(itr->second > deauthCountMax)
+      {
+        deauthCountMax = itr->second; 
+      }
+//      Serial.print("deauth count Max=");
+//      Serial.println( deauthCountMax);
+//      Serial.print("---------------");
+    }
+    else if(trainingend == true)
+    {
+      if(itr->second > deauthCountMax)
+      {
+        found = true;
+        period = 5;
+        Serial.println("Alert now deauth =");
+        Serial.println(itr->second);
+      }
+    }
   }
      
   }
@@ -73,11 +100,37 @@ void sniffer(uint8_t *buf, uint16_t len) {
     {
       //Serial.print(itr2->second);
       //Serial.print(" ");
-      if(itr2->second > 700)
-      {
-        found = true;  
-      }
+//      if(itr2->second > 700)
+//      {
+//        found = true;  
+//      }
       mem[addr_b] = itr2->second+1;
+
+      if(trainingend == false)
+    {
+//      Serial.println("---------------");
+//      Serial.print(" traing count = ");
+//      Serial.println(count);
+//      Serial.print(" beacon count = ");
+//      Serial.println(itr2->second);
+      if(itr2->second > beaconCountMax)
+      {
+        beaconCountMax = itr2->second; 
+      }
+//      Serial.print("beacon count Max=");
+//      Serial.println( beaconCountMax);
+//      Serial.println("---------------");
+    }
+    else if(trainingend == true)
+    {
+      if(itr2->second > beaconCountMax * 5)
+      {
+        found = true;
+        period = 5;
+        Serial.println("Alert now beacon =");
+        Serial.println(itr2->second);
+      }
+    }
     }
   }
   if(pkt_type == 0x40)
@@ -91,11 +144,36 @@ void sniffer(uint8_t *buf, uint16_t len) {
     {
 //      Serial.print(itr3->second);
 //      Serial.print(" ");
-      if(itr3->second > 50)
-      {
-        found = true;  
-      }
+//      if(itr3->second > 50)
+//      {
+//        found = true;  
+//      }
       memforprobe[addr_a] = itr3->second+1;
+      if(trainingend == false)
+    {
+//      Serial.println("---------------");
+//      Serial.print(" traing count = ");
+//      Serial.println(count);
+//      Serial.print(" probe count = ");
+//      Serial.println(itr3->second);
+      if(itr3->second > probeCountMax)
+      {
+        probeCountMax = itr3->second; 
+      }
+//      Serial.print("probe count Max=");
+//      Serial.println( probeCountMax);
+//      Serial.println("---------------");
+    }
+    else if(trainingend == true)
+    {
+      if(itr3->second > probeCountMax * 2)
+      {
+        found = true;
+        period = 5;
+        Serial.println("Alert now probe =");
+        Serial.println(itr3->second);
+      }
+    }
     }
   }
   
@@ -133,34 +211,70 @@ void loop() {
   unsigned long current_time = millis(); // Get current time (in ms)
   
   // Update each second (or scan-time-per-channel * channel-range)
-  if (current_time - update_time >= (sizeof(channels)*CH_TIME)) {
+  if (current_time - update_time >= (sizeof(channels)*CH_TIME)) { 
     update_time = current_time; // Update time variable
 
     
     //****When detected deauth packets exceed the minimum allowed number*****
 
-    if(period >= 5)
+    if(trainingend == false)
     {
-      if(found == true)
-      {
-        attack_started();
-      }
-      else
-      {
-        attack_stopped();
-      }
-
+      count++;
+      Serial.println("count=");
+      Serial.println(count);
+      Serial.println("deauthMax=");
+      Serial.println(deauthCountMax);
+      Serial.println("beaconMax=");
+      Serial.println(beaconCountMax);
+      Serial.println("probeMax=");
+      Serial.println(probeCountMax);
       mem.clear();
       newdata.clear();
       memforprobe.clear();
-      found = false;
-      period = 0;
+      if(count == 10)
+      {
+        if(deauthCountMax < 10)
+        {
+          deauthCountMax = 10;
+        }
+        if(beaconCountMax < 50)
+        {
+          beaconCountMax = 50;
+        }
+        if(probeCountMax < 30)
+        {
+          probeCountMax = 30;
+        }
+        trainingend = true;
+      }
     }
+    else if(trainingend == true)
+    {
+      if(period >= 5)
+      {
+        if(found == true)
+        {
+          
+          attack_started();
+        }
+        else
+        {
+          attack_stopped();
+        }
+
+        mem.clear();
+        newdata.clear();
+        memforprobe.clear();
+        found = false;
+        period = 0;
+     }
      
-    period++;
+     period++;
     
     
   }
+  
+    
 
   //**************Channel hopping************************************
   if (sizeof(channels) > 1 && current_time - ch_time >= CH_TIME) {
@@ -176,4 +290,5 @@ void loop() {
     wifi_set_channel(ch);
   }
 
+  }
 }
